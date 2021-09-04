0776ee26ca6634dd622c56be3ce742e4e451b3e9
aciobanusebi <aciobanusebi@gmail.com>
2021-09-04 21:31:21 +0300

    Add files via upload

--- run_main_large_datasets.py
+++ run_main_large_datasets.py
@@ -1,13 +1,16 @@
 import os
 
-OUTPUT_DIRECTORY = "results"
+OUTPUT_DIRECTORY = "../results"
 
 n_init = 10
 
 for original_data_name in ["mnist", "mnist5", "fmnist", "cifar10"]:
+  directory = OUTPUT_DIRECTORY + "/" + original_data_name
+  if not os.path.exists(directory):
+    os.makedirs(directory)
   for dim_pca in [100, None]:
     for it in range(n_init):
-      filename = OUTPUT_DIRECTORY + "/" + original_data_name + "_" + str(dim_pca) + "_" + str(it) + ".txt"
-      command = "python main.py " + " " +original_data_name + " " + str(dim_pca) + " " + str(it) + " > " + filename
+      filename = directory + "_" + str(dim_pca) + "_" + str(it) + ".txt"
+      command = "python main_large_datasets.py " + " " +original_data_name + " " + str(dim_pca) + " " + str(it) + " > " + filename
       print(command)
       os.system(command)

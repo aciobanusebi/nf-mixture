eb371942bdcec3a853c871667b9aa78fdd0115fd
aciobanusebi <aciobanusebi@gmail.com>
2021-09-04 21:21:26 +0300

    Add files via upload

--- run_main_toy_datasets.py
+++ run_main_toy_datasets.py
@@ -1,6 +1,6 @@
 import os
 
-OUTPUT_DIRECTORY = "../results"
+OUTPUT_DIRECTORY = "results"
 
 n_init = 10
 
@@ -8,7 +8,7 @@ for original_data_name in ["two_banana","smile","moons","circles","pinwheel"]:
   for it in range(n_init):
     directory = OUTPUT_DIRECTORY + "/" +  original_data_name
     if not os.path.exists(directory):
-      os.mkdir(directory)
+      os.makedirs(directory)
     filename = directory + "/" + str(it) + ".txt"
     command = "python main_toy_datasets.py " + " " + original_data_name + " "  + str(it) + " " + directory + " > " + filename
     print(command)
